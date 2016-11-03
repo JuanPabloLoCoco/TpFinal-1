df90998ff9ed10ed1d7645c51a4a7c877c800ccb
MicaBanfi <micabanfi@hotmail.com>
2016-11-03 19:31:52 -0300

    Arreglo while

--- Hacerjugada.c
+++ Hacerjugada.c
@@ -1,10 +1,10 @@
-int hacerjugada(**matriz, int dim, int f1, int c1, int f2, int c2 )
+int hacerjugada(**matriz, int dim, int F1, int C1, int F2, int C2 )
 {
-  int dx,dy,x=f1,y=c1, cont=0 ;
-  dx=(f2-f1)/fabs(f2-f1);
-  dy=(c2-c1)/fabs(c2-c1);
+  int dx,dy,x=F1,y=C1, cont=0 ;
+  dx=(F2-F1)/fabs(F2-F1);
+  dy=(C2-C1)/fabs(C2-C1);
 
-  while(f2==x && c2==y)
+  while(F2!=x &&C2!=y)
   {
     if ( matriz[x][y]!="")
     {
